9b61369b222dcf54cb46dedbb6161eb7fde85948
Nicholas Andrews <noa@jhu.edu>
2020-05-06 00:24:00 -0400

    wip

--- fairseq_cli/validate.py
+++ fairseq_cli/validate.py
@@ -7,6 +7,8 @@
 
 import logging
 import sys
+import os
+import numpy as np
 
 import torch
 
@@ -32,11 +34,20 @@ def main(args, override_args=None):
     use_fp16 = args.fp16
     use_cuda = torch.cuda.is_available() and not args.cpu
 
-    if override_args is not None:
-        overrides = vars(override_args)
-        overrides.update(eval(getattr(override_args, 'model_overrides', '{}')))
-    else:
-        overrides = None
+    if use_cuda:
+        torch.cuda.set_device(args.device_id)
+
+    print(f"Torch see CUDA? {torch.cuda.is_available()}")
+    print(f"Use CPU? {args.cpu}")
+    print(f"Use CUDA? {use_cuda}")
+
+    # if override_args is not None:
+    #     print(override_args)
+    #     overrides = vars(override_args)
+    #     overrides.update(eval(getattr(override_args, 'model_overrides', '{}')))
+    # else:
+    #     overrides = None
+    overrides = {'criterion': 'cross_entropy'}
 
     # Load ensemble
     logger.info('loading model(s) from {}'.format(args.path))
@@ -45,13 +56,26 @@ def main(args, override_args=None):
         arg_overrides=overrides,
         suffix=getattr(args, "checkpoint_suffix", ""),
     )
+    print("WARNING: Just taking first model from ensemble")
     model = models[0]
+
+    # If we're obtaining full distributions, we need to serialize them
+    # in binary format. We use this strategy:
+    # https://stackoverflow.com/questions/47493409/streaming-multiple-numpy-arrays-to-a-file
+    if args.print_full_dist:
+        assert args.full_dist_path
+        import pickle
+        if os.path.exists(args.full_dist_path):
+            print(f"Deleting existing file: {args.full_dist_path}")
+            os.remove(args.full_dist_path)
+        dist_output_file = open(args.full_dist_path, 'ab')
 
     # Move models to GPU
     for model in models:
         if use_fp16:
             model.half()
         if use_cuda:
+            print("Using CUDA")
             model.cuda()
 
     # Print args
@@ -69,6 +93,7 @@ def main(args, override_args=None):
             raise Exception('Cannot find dataset: ' + subset)
 
         # Initialize data iterator
+        print(f"Num workers: {args.num_workers}")
         itr = task.get_batch_iterator(
             dataset=dataset,
             max_tokens=args.max_tokens,
@@ -89,19 +114,43 @@ def main(args, override_args=None):
             prefix=f"valid on '{subset}' subset",
             default_log_format=('tqdm' if not args.no_progress_bar else 'simple'),
         )
-
+        means = []
         log_outputs = []
         for i, sample in enumerate(progress):
             sample = utils.move_to_cuda(sample) if use_cuda else sample
-            _loss, _sample_size, log_output = task.valid_step(sample, model, criterion)
-            progress.log(log_output, step=i)
-            log_outputs.append(log_output)
+            if args.print_full_dist:
+                ids = sample['id'].cpu().numpy()
+                n_sentences = ids.shape[0]
+                # IMPORTANT NOTE: Padding is used for sents of diff len
+                #print(sample['target'].shape)
+                #print(criterion.padding_idx)
+                lprobs = task.predict_step(sample, model, criterion)
+                vals, inds = lprobs.topk(8)
+                print(vals.shape)
+                print(inds.shape)
+                lmass = vals.logsumexp(2)
+                #print(m32.exp().mean())
+                means.append(lmass.exp().mean().cpu().numpy())
+                print(np.mean(means))
+                #t64 = lprobs.topk(lprobs, 64)
+                #t128 = lprobs.topk(lprobs, 128)
+                lprobs = lprobs.cpu().numpy()
+                print(lprobs.shape)
+                for j in range(n_sentences):
+                    pickle.dump((ids[j], lprobs[j]), dist_output_file)
+            else:
+                _loss, _sample_size, log_output = task.valid_step(sample, model, criterion)
+                progress.log(log_output, step=i)
+                log_outputs.append(log_output)
 
-        with metrics.aggregate() as agg:
-            task.reduce_metrics(log_outputs, criterion)
-            log_output = agg.get_smoothed_values()
+        if args.print_full_dist:
+            dist_output_file.close()
+        else:
+            with metrics.aggregate() as agg:
+                task.reduce_metrics(log_outputs, criterion)
+                log_output = agg.get_smoothed_values()
 
-        progress.print(log_output, tag=subset, step=i)
+            progress.print(log_output, tag=subset, step=i)
 
 
 def cli_main():
@@ -112,7 +161,8 @@ def cli_main():
     # only override args that are explicitly given on the command line
     override_parser = options.get_validation_parser()
     add_distributed_training_args(override_parser)
-    override_args = options.parse_args_and_arch(override_parser, suppress_defaults=True)
+    override_args = options.parse_args_and_arch(override_parser,
+                                                suppress_defaults=True)
 
     distributed_utils.call_main(args, main, override_args=override_args)
 
