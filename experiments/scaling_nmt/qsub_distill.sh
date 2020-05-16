47c21ef43b9e79afe6128fb748099507eaf1ea78
Nicholas Andrews <noa@jhu.edu>
2020-05-16 02:03:10 -0400

    token distillation

---
+++ experiments/scaling_nmt/qsub_distill.sh
@@ -0,0 +1,86 @@
+#! /usr/bin/env bash
+
+set -e
+set -u
+
+# --- SYSTEM ---
+N_GPU=2  # args must be adjusted below if this is changed
+UPDATE_FREQ=4
+MAX_UPDATE=300000
+GPU_TYPE=2080
+NUM_PROC=20
+MEM=12G
+HOURS=48
+
+if [ $# -lt 2 ]; then
+   echo "Usage: ${0} JOB_NAME TEACHER [FLAGS]"
+   exit
+fi
+
+TASK=en_de
+JOB_NAME=${1}
+TEACHER=${2}
+shift
+shift
+
+
+DATA_DIR=/expscratch/nandrews/nmt/fairseq/data/wmt16_en_de_bpe32k
+if [ ! -d "${DATA_DIR}" ]; then
+    echo "${DATA_DIR} does not exist"
+    exit
+fi
+
+JOB_DIR=/expscratch/${USER}/nmt/fairseq/jobs/scaling_nmt_distill/${JOB_NAME}
+mkdir -p ${JOB_DIR}
+JOB_SCRIPT=${JOB_DIR}/job.sh
+
+TRAIN="fairseq-train"
+
+# Write training script
+cat >${JOB_SCRIPT} <<EOL
+#$ -cwd
+#$ -V
+#$ -w e
+#$ -N ${JOB_NAME}
+#$ -m bea
+#$ -j y
+#$ -o ${JOB_DIR}/out
+#$ -e ${JOB_DIR}/err
+
+# Stop on error
+set -e
+set -u
+set -f
+
+module load cuda10.1/toolkit
+module load cuda10.1/blas
+module load cudnn/7.6.3_cuda10.1
+module load nccl/2.4.7_cuda10.1
+export MKL_SERVICE_FORCE_INTEL=1
+
+fairseq-train \
+    ${DATA_DIR} \
+    --task translation_with_teacher \
+    --teacher-pred ${TEACHER} \
+    --arch transformer_wmt_en_de --share-all-embeddings \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+    --lr 0.0007 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 \
+    --weight-decay 0.0 --dropout 0.1 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --no-progress-bar \
+    --save-dir ${JOB_DIR} \
+    --max-tokens 4096 \
+    --fp16 \
+    --keep-last-epochs 10 \
+    --update-freq ${UPDATE_FREQ} \
+    --max-update ${MAX_UPDATE} $@
+
+EOL
+
+chmod a+x ${JOB_SCRIPT}
+#QSUB_CMD="qsub -q gpu.q@@${GPU_TYPE} -l gpu=${N_GPU},mem_free=${MEM},h_rt=${HOURS}:00:00,num_proc=${NUM_PROC} ${JOB_SCRIPT}"
+#echo ${QSUB_CMD}
+#${QSUB_CMD}
+bash ${JOB_SCRIPT}
+
+# eof
---
+++ fairseq/data/language_pair_dataset_with_teacher.py
@@ -0,0 +1,178 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+
+import numpy as np
+import torch
+
+from . import data_utils, FairseqDataset, LanguagePairDataset
+
+logger = logging.getLogger(__name__)
+
+
+def collate(
+    samples, pad_idx, eos_idx, left_pad_source=True, left_pad_target=False,
+    input_feeding=True,
+):
+    if len(samples) == 0:
+        return {}
+
+    def merge(key, left_pad, move_eos_to_beginning=False):
+        return data_utils.collate_tokens(
+            [s[key] for s in samples],
+            pad_idx, eos_idx, left_pad, move_eos_to_beginning,
+        )
+
+    def check_alignment(alignment, src_len, tgt_len):
+        if alignment is None or len(alignment) == 0:
+            return False
+        if alignment[:, 0].max().item() >= src_len - 1 or alignment[:, 1].max().item() >= tgt_len - 1:
+            logger.warning("alignment size mismatch found, skipping alignment!")
+            return False
+        return True
+
+    def compute_alignment_weights(alignments):
+        """
+        Given a tensor of shape [:, 2] containing the source-target indices
+        corresponding to the alignments, a weight vector containing the
+        inverse frequency of each target index is computed.
+        For e.g. if alignments = [[5, 7], [2, 3], [1, 3], [4, 2]], then
+        a tensor containing [1., 0.5, 0.5, 1] should be returned (since target
+        index 3 is repeated twice)
+        """
+        align_tgt = alignments[:, 1]
+        _, align_tgt_i, align_tgt_c = torch.unique(align_tgt, return_inverse=True, return_counts=True)
+        align_weights = align_tgt_c[align_tgt_i[np.arange(len(align_tgt))]]
+        return 1. / align_weights.float()
+
+    id = torch.LongTensor([s['id'] for s in samples])
+    src_tokens = merge('source', left_pad=left_pad_source)
+    # sort by descending source length
+    src_lengths = torch.LongTensor([s['source'].numel() for s in samples])
+    src_lengths, sort_order = src_lengths.sort(descending=True)
+    id = id.index_select(0, sort_order)
+    src_tokens = src_tokens.index_select(0, sort_order)
+
+    prev_output_tokens = None
+    target = None
+    if samples[0].get('target', None) is not None:
+        target = merge('target', left_pad=left_pad_target)
+        target = target.index_select(0, sort_order)
+        tgt_lengths = torch.LongTensor([s['target'].numel() for s in samples]).index_select(0, sort_order)
+        ntokens = sum(len(s['target']) for s in samples)
+
+        if input_feeding:
+            # we create a shifted version of targets for feeding the
+            # previous output token(s) into the next decoder step
+            prev_output_tokens = merge(
+                'target',
+                left_pad=left_pad_target,
+                move_eos_to_beginning=True,
+            )
+            prev_output_tokens = prev_output_tokens.index_select(0, sort_order)
+    else:
+        ntokens = sum(len(s['source']) for s in samples)
+
+    batch = {
+        'id': id,
+        'nsentences': len(samples),
+        'ntokens': ntokens,
+        'net_input': {
+            'src_tokens': src_tokens,
+            'src_lengths': src_lengths,
+        },
+        'target': target,
+    }
+    if prev_output_tokens is not None:
+        batch['net_input']['prev_output_tokens'] = prev_output_tokens
+
+    if samples[0].get('alignment', None) is not None:
+        bsz, tgt_sz = batch['target'].shape
+        src_sz = batch['net_input']['src_tokens'].shape[1]
+
+        offsets = torch.zeros((len(sort_order), 2), dtype=torch.long)
+        offsets[:, 1] += (torch.arange(len(sort_order), dtype=torch.long) * tgt_sz)
+        if left_pad_source:
+            offsets[:, 0] += (src_sz - src_lengths)
+        if left_pad_target:
+            offsets[:, 1] += (tgt_sz - tgt_lengths)
+
+        alignments = [
+            alignment + offset
+            for align_idx, offset, src_len, tgt_len in zip(sort_order, offsets, src_lengths, tgt_lengths)
+            for alignment in [samples[align_idx]['alignment'].view(-1, 2)]
+            if check_alignment(alignment, src_len, tgt_len)
+        ]
+
+        if len(alignments) > 0:
+            alignments = torch.cat(alignments, dim=0)
+            align_weights = compute_alignment_weights(alignments)
+
+            batch['alignments'] = alignments
+            batch['align_weights'] = align_weights
+
+    return batch
+
+
+class LanguagePairDatasetWithTeacher(LanguagePairDataset):
+    """
+    A pair of torch.utils.data.Datasets.
+
+    Args:
+        src (torch.utils.data.Dataset): source dataset to wrap
+        src_sizes (List[int]): source sentence lengths
+        src_dict (~fairseq.data.Dictionary): source vocabulary
+        tgt (torch.utils.data.Dataset, optional): target dataset to wrap
+        tgt_sizes (List[int], optional): target sentence lengths
+        tgt_dict (~fairseq.data.Dictionary, optional): target vocabulary
+        left_pad_source (bool, optional): pad source tensors on the left side
+            (default: True).
+        left_pad_target (bool, optional): pad target tensors on the left side
+            (default: False).
+        max_source_positions (int, optional): max number of tokens in the
+            source sentence (default: 1024).
+        max_target_positions (int, optional): max number of tokens in the
+            target sentence (default: 1024).
+        shuffle (bool, optional): shuffle dataset elements before batching
+            (default: True).
+        input_feeding (bool, optional): create a shifted version of the targets
+            to be passed into the model for teacher forcing (default: True).
+        remove_eos_from_source (bool, optional): if set, removes eos from end
+            of source if it's present (default: False).
+        append_eos_to_target (bool, optional): if set, appends eos to end of
+            target if it's absent (default: False).
+        align_dataset (torch.utils.data.Dataset, optional): dataset
+            containing alignments.
+        append_bos (bool, optional): if set, appends bos to the beginning of
+            source/target sentence.
+        teacher_file: file with teacher predictions
+
+    """
+
+    def __init__(
+        self, src, src_sizes, src_dict,
+        tgt=None, tgt_sizes=None, tgt_dict=None,
+        left_pad_source=True, left_pad_target=False,
+        max_source_positions=1024, max_target_positions=1024,
+        shuffle=True, input_feeding=True,
+        remove_eos_from_source=False, append_eos_to_target=False,
+        align_dataset=None,
+        append_bos=False, eos=None,
+        teacher_file=None
+    ):
+        super().__init__(src, src_sizes, src_dict, tgt=tgt,
+                         tgt_sizes=tgt_sizes, tgt_dict=tgt_dict,
+                         left_pad_source=left_pad_source,
+                         left_pad_target=left_pad_target,
+                         max_source_positions=max_source_positions,
+                         max_target_positions=max_target_positions,
+                         shuffle=shuffle, input_feeding=input_feeding,
+                         remove_eos_from_source=remove_eos_from_source,
+                         append_eos_to_target=append_eos_to_target,
+                         align_dataset=align_dataset, append_bos=append_bos,
+                         eos=eos)
+
+        self.teacher_file = teacher_file
